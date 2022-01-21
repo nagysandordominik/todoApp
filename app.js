652eb489b70cd07736c2ca4a7aee341b070b2a3c
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-01-21 20:43:24 +0100

    change: html template modified

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const todoInput = document.getElementById('inputT');
 const addButton = document.getElementById('addB');
 const todoList = document.getElementById('todoList');
-const here = document.getElementById('here');
+
 
 if(addButton){
   addButton.addEventListener('click', addToDo);
@@ -11,6 +11,15 @@ function addToDo(event) {
   event.preventDefault();
   let text = todoInput.value;
   const newTodo = document.createElement('p')
-  newTodo.innerHTML =  `<p id="here" class="d-inline"><input class="form-check-input me-2" type="checkbox" value="" aria-label="..." />${text}</p>`;
+  newTodo.innerHTML =  
+  `<ul class="list-group mb-0" id="todoList">
+    <li class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+     <div class="d-flex align-items-center" id="here">
+       <p id="here" class="d-inline"><input class="form-check-input me-2" type="checkbox" value="" aria-label="..." />${text}</p>
+     </div>
+     <a href="#!" data-mdb-toggle="tooltip" title="Remove item">
+       <i class="fas fa-times text-primary"></i>
+     </a>
+    </li>`;
   todoList.appendChild(newTodo);
 }

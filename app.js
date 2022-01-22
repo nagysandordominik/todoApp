c9658ebb78980b20c603bc74de343f6be3a9a582
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-01-22 19:00:06 +0100

    fix: addtodo event listener made simpler, and deletetodo function is halfway solved

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const todoInput = document.getElementById('inputT');
 const addButton = document.getElementById('addB');
 const todoList = document.getElementById('todoList');
-let checkBoxes = document.querySelectorAll('input[type=checkbox]');
 
-  addButton.addEventListener('click', addToDo);
-function addToDo(event) {
+
+// Task added to list via input typed by user
+addButton.addEventListener('click', addToDo => {
   event.preventDefault();
   const newTodo = document.createElement('p')
   newTodo.innerHTML =  
   `<ul class="list-group mb-0" id="todoList">
     <li class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
      <div class="d-flex align-items-center" id="here">
-       <p id="here" class="d-inline"><input class="form-check-input me-2" type="checkbox" value="" aria-label="..." />${todoInput.value}</p>
+       <p id="here" class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="deleteToDo()" value="" aria-label="..." />${todoInput.value}</p>
      </div>
      <a href="#!" data-mdb-toggle="tooltip" title="Remove item">
        <i class="fas fa-times text-primary"></i>
@@ -19,23 +19,10 @@ function addToDo(event) {
     </li>`;
   todoList.appendChild(newTodo);
   todoInput.value = '';
+});
+
+// Checked checkboxes make todo texts line through
+function deleteToDo () {
+  let text = document.querySelector('p');
+  text.classList.add('text-decoration-line-through');
 }
-
-for (let i = 0; i < checkBoxes.length; i++) {
-  checkBoxes[i].addEventListener("change", checkedOrNot);
-}
-
-//Define separate function
-function checkedOrNot() {
-  let isChecked = this.checked;
-
-  if (isChecked) { //checked
-    console.log('checked');
-  } else { //unchecked
-    console.log('unchecked');
-  }
-}
-
-
-  
-

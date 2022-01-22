28f6b4f9b712db66eb67ea9857038af9fd08e6ad
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-01-22 19:47:12 +0100

    add: id generator for added tasks, and selecting them with specific ids

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const todoInput = document.getElementById('inputT');
 const addButton = document.getElementById('addB');
 const todoList = document.getElementById('todoList');
+const item = document.getElementsByTagName('p');
 
+function randomId(item) {
+  let S4 = function() {
+     return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  };
+  return (S4()+S4());
+}
 
 // Task added to list via input typed by user
 addButton.addEventListener('click', addToDo => {
@@ -10,8 +17,8 @@ addButton.addEventListener('click', addToDo => {
   newTodo.innerHTML =  
   `<ul class="list-group mb-0" id="todoList">
     <li class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-     <div class="d-flex align-items-center" id="here">
-       <p id="here" class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="deleteToDo()" value="" aria-label="..." />${todoInput.value}</p>
+     <div class="d-flex align-items-center" >
+       <p id="${randomId(item)}" class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="deleteToDo()"  value="" aria-label="..." />${todoInput.value}</p>
      </div>
      <a href="#!" data-mdb-toggle="tooltip" title="Remove item">
        <i class="fas fa-times text-primary"></i>
@@ -22,7 +29,9 @@ addButton.addEventListener('click', addToDo => {
 });
 
 // Checked checkboxes make todo texts line through
-function deleteToDo () {
-  let text = document.querySelector('p');
-  text.classList.add('text-decoration-line-through');
+function deleteToDo (id) {
+  item.id = randomId(id);
+  if (item.id === randomId) {
+    item.classList.toggle('text-decoration-line-through');
+  }
 }

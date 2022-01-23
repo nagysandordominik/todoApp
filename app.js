5616e8322d24769b8869f3e8fa416e3cd4ebee09
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-01-23 19:57:27 +0100

    fix: removed unused ul, and changed createlement to div

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const todoList = document.getElementById('todoList');
 // Task added to list via input typed by user
 addButton.addEventListener('click', addToDo => {
   event.preventDefault();
-  const newTodo = document.createElement('p')
+  const newTodo = document.createElement('div')
   newTodo.innerHTML =  
-  `<ul class="list-group mb-0" id="todoList">
-    <li class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+  `<li class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
      <div class="d-flex align-items-center" >
        <p id="" class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="deleteToDo(event)" />${todoInput.value}</p>
      </div>
@@ -26,4 +25,5 @@ addButton.addEventListener('click', addToDo => {
 function deleteToDo(event) {
   let checkbox = event.target;
   checkbox.parentNode.classList.toggle('text-decoration-line-through');
+
   }

a0931d8a72c501be9df94f83ba07c7762a6e0a96
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-01-27 18:47:49 +0100

    add: adding tasks into an array, storing with text and id attr

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const todoInput = document.getElementById('inputT');
 const addButton = document.getElementById('addB');
 const todoList = document.getElementById('todoList');
-let newTodo = [];
+const todoTaskList  = [];
 
 
 // Task added to list via input typed by user
@@ -10,19 +10,24 @@ addButton.addEventListener('click', addToDo => {
   if (todoInput.value == '') {
     alert ('You have to type something') 
   } else {
-    newTodo = document.createElement('div');
+    const todoTask = {
+      id:todoTaskList.length,
+      todoText: todoInput.value,
+    }
+    let newTodo = document.createElement('div');
     newTodo.innerHTML =  
-  `<li class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-       <p class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="deleteToDo(event)"/>${todoInput.value}</p>
+  `<li id="${todoTask.id}" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+       <p class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="completedToDo(event)"/>${todoInput.value}</p>
     </li>`;
   todoList.appendChild(newTodo);
+  todoTaskList.unshift(todoTask);
   todoInput.value = '';
   }
 });
 
 // Checked checkboxes make todo texts line through
 
-function deleteToDo(event) {
+function completedToDo(event) {
   event.target.parentNode.classList.toggle('text-decoration-line-through');
   setTimeout(function() {event.target.parentNode.parentNode.remove();}, 3000);
   }

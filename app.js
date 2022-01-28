60dee0d33a4ec5ec371968811db61c61a526bf58
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-01-28 20:03:17 +0100

    add: randomid for every task added

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const todoInput = document.getElementById('inputT');
 const addButton = document.getElementById('addB');
 const todoList = document.getElementById('todoList');
 const todoTaskList  = [];
+const completedToDo = [];
 
+function randomId(item) {
+  let S4 = function() {
+     return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  };
+  return (S4()+S4());
+}
 
 // Task added to list via input typed by user
 addButton.addEventListener('click', addToDo => {
@@ -11,14 +18,16 @@ addButton.addEventListener('click', addToDo => {
     alert ('You have to type something') 
   } else {
     const todoTask = {
-      id:todoTaskList.length,
+      id: randomId(),
       todoText: todoInput.value,
     }
-    let newTodo = document.createElement('div');
+    const newTodo = document.createElement('div');
     newTodo.innerHTML =  
-  `<li id="${todoTask.id}" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-       <p class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="completedToDo(event)"/>${todoInput.value}</p>
-    </li>`;
+    `<li id="${todoTask.id}" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
+     <p class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="completeToDo(event)"/>${todoInput.value}</p>
+    </li>`
+    
+    
   todoList.appendChild(newTodo);
   todoTaskList.unshift(todoTask);
   todoInput.value = '';
@@ -26,12 +35,16 @@ addButton.addEventListener('click', addToDo => {
 });
 
 // Checked checkboxes make todo texts line through
-
-function completedToDo(event) {
-  event.target.parentNode.classList.toggle('text-decoration-line-through');
+function completeToDo(event) {
+  const selectedText = event.target.parentNode;
+  const selectedTask = event.target.parentNode.parentNode;
+  selectedText.classList.toggle('text-decoration-line-through');
+  // With timeout the selected task is removed from display and todoTaskList, than it is added to completedTodo array 
   setTimeout(function() {
-    event.target.parentNode.parentNode.remove();
-    todoTaskList.splice(event.target.parentNode.parentNode,1)
+    const selectedId = event.target.parentNode.parentNode.id;
+    selectedTask.remove(todoList);
+    const completedTask = todoTaskList.splice(selectedTask[selectedId],1);
+    completedToDo.push(completedTask)
   }, 3000);
   }
   

2e80d877bf832e1ecace631573f4683299425792
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-02-03 17:48:48 +0100

    fix: added task with padding/margin 1 value and rename todoTaskList array

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const todoInput = document.getElementById('inputT');
 const addButton = document.getElementById('addB');
 const todoList = document.getElementById('todoList');
-let todoTaskList  = [];
+const menus = document.querySelectorAll('a');
+
+// Arrays for 3 types of tasks 
+let activeList  = [];
 let completedList = [];
 let allTasks = [];
 
+
+// Random generated id for tasks
 function randomId() {
   let S4 = function() {
      return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
@@ -16,7 +21,7 @@ function randomId() {
 addButton.addEventListener('click', addToDo => {
   event.preventDefault();
   if (todoInput.value == '') {
-    alert ('You have to type something') 
+    alert ('You have to type something');
   } else {
     const todoTask = {
       id: randomId(),
@@ -25,38 +30,40 @@ addButton.addEventListener('click', addToDo => {
     }
     const newTodo = document.createElement('div');
     newTodo.innerHTML =  
-    `<li id="${todoTask.id}" class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
-     <p class="d-inline "><input class="form-check-input me-2" type="checkbox" onclick="completeToDo(event)"/>${todoInput.value}</p>
+    `<li id="${todoTask.id}" class="list-group-item justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom pb-0 mb-0">
+     <p class="pb-1 mb-1"><input class="form-check-input me-2" type="checkbox" onclick="completeToDo(event)"/>${todoInput.value}</p>
     </li>`
     
     
   todoList.appendChild(newTodo);
-  todoTaskList.push(todoTask);
+  activeList.push(todoTask);
   allTasks.push(todoTask);
   todoInput.value = '';
   }
 });
-// Checked checkboxes make todo texts line through
-function completeToDo(event) {
+
+  // Checked checkboxes make todo texts line through
+  function completeToDo(event) {
   let selectedText = event.target.parentNode;
   selectedText.classList.toggle('text-decoration-line-through');
   let selectedId = event.target.parentNode.parentNode.id;
   let selectedDiv = event.target.parentNode.parentNode.parentNode;
+
   // With timeout the selected task is removed from display and todoTaskList, than it is added to completedList array 
   setTimeout(function() {
-    let index = todoTaskList.map(todoTask => {
+    let index = activeList.map(todoTask => {
       return todoTask.id;}).indexOf(selectedId);
-      todoTaskList[index].isDone
-      ? (todoTaskList[index].isDone = false)
-      : (todoTaskList[index].isDone = true);
+      activeList[index].isDone
+      ? (activeList[index].isDone = false) 
+      : (activeList[index].isDone = true);
     
       const completedTask = {
         id: selectedId,
         text: selectedText.innerText,
-        isDone:todoTaskList[index].isDone
+        isDone:activeList[index].isDone
       };
     
-    todoTaskList.splice(index, 1);
+      activeList.splice(index, 1);
     completedList.push(completedTask);
     selectedDiv.remove(todoList);
   }, 3000);
